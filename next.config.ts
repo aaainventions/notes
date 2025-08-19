fa67d158ae9442d3889819a283c1a1ba2eebb81f
aaainventions <mkhancyb3r@gmail.com>
2025-08-19 11:53:12 -0700

    fixing

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
 
 export default nextConfig;
